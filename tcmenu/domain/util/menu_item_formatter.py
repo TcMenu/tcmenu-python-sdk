2f85b4b36e2cf211f7104a9a7ff0ca3a4b9ec368
Vladimír Záhradník <vladimir@zahradnik.io>
2023-01-08 11:59:08 +0100

    Implement MenuItemFormatter

--- tcmenu/domain/util/menu_item_formatter.py
+++ tcmenu/domain/util/menu_item_formatter.py
@@ -1,5 +1,7 @@
 from typing import Any, Optional
+import re
 
+from tcmenu.domain.edit_item_type import EditItemType
 from tcmenu.domain.menu_items import (
     MenuItem,
     ScrollChoiceMenuItem,
@@ -18,31 +20,80 @@ from tcmenu.domain.state.portable_color import PortableColor
 class MenuItemFormatter:
     @staticmethod
     def format_to_wire(item: MenuItem, text: str) -> str:
-        pass
+        if isinstance(item, AnalogMenuItem):
+            return MenuItemFormatter._format_analog_to_wire(text)
+        elif isinstance(item, EnumMenuItem):
+            return MenuItemFormatter._format_enum_to_wire(text)
+        elif isinstance(item, BooleanMenuItem):
+            return MenuItemFormatter._format_bool_to_wire(text)
+        elif isinstance(item, EditableLargeNumberMenuItem):
+            return MenuItemFormatter._format_large_num_to_wire(text)
+        elif isinstance(item, Rgb32MenuItem):
+            return MenuItemFormatter._format_rgb_item_to_wire(text)
+        elif isinstance(item, ScrollChoiceMenuItem):
+            return MenuItemFormatter._format_scroll_item_to_wire(text)
+        elif isinstance(item, EditableTextMenuItem):
+            return MenuItemFormatter._format_editable_text_to_wire(item, text)
+        else:
+            raise ValueError(f"Item {item.name} is not editable.")
 
     @staticmethod
-    def _format_editable_text_to_wire() -> str:
-        pass
+    def _format_editable_text_to_wire(item: EditableTextMenuItem, text: str) -> str:
+        if item.item_type == EditItemType.PLAIN_TEXT and len(text) < item.text_length:
+            return text
+        elif item.item_type == EditItemType.IP_ADDRESS:
+            return "0.0.0.0" if not re.match("\\d+\\.\\d+\\.\\d+\\.\\d+", text) else text
+        elif (
+            item.item_type == EditItemType.TIME_24H
+            or item.item_type == EditItemType.TIME_24_HUNDREDS
+            or item.item_type == EditItemType.TIME_12H
+        ):
+            # Time is always sent back to the server in 24-hour format. It is always possible
+            # (but optional) to provide hundreds/sec.
+            return "12:00:00" if not re.match("\\d+:\\d+:\\d+(.\\d*)*", text) else text
+        elif item.item_type == EditItemType.GREGORIAN_DATE:
+            return "01/01/2000" if not re.match("\\d+/\\d+/\\d+", text) else text
+        else:
+            return ""
 
     @staticmethod
-    def _format_large_num_to_wire() -> str:
-        pass
+    def _format_large_num_to_wire(text: str) -> str:
+        try:
+            val = int(text)
+        except ValueError:
+            val = 0
+
+        return str(val)
 
     @staticmethod
-    def _format_scroll_item_to_wire() -> str:
-        pass
+    def _format_rgb_item_to_wire(text: str) -> str:
+        return str(PortableColor.from_html(text))
 
     @staticmethod
-    def _format_bool_to_wire() -> str:
-        pass
+    def _format_scroll_item_to_wire(text: str) -> str:
+        try:
+            val = int(text)
+        except ValueError:
+            val = 0
+
+        return str(CurrentScrollPosition(val, text))
 
     @staticmethod
-    def _format_enum_to_wire() -> str:
-        pass
+    def _format_bool_to_wire(text: str) -> str:
+        text = text.upper()
+
+        if text == "ON" or text == "YES" or text == "TRUE":
+            return "1"
+        else:
+            return "0"
 
     @staticmethod
-    def _format_analog_to_wire() -> str:
-        pass
+    def _format_enum_to_wire(text: str) -> str:
+        return text
+
+    @staticmethod
+    def _format_analog_to_wire(text: str) -> str:
+        return text
 
     @staticmethod
     def _get_actual_decimal_divisor(divisor: int) -> int:
@@ -70,30 +121,30 @@ class MenuItemFormatter:
         elif isinstance(item, EnumMenuItem):
             return MenuItemFormatter._format_enum_for_display(item, data)
         elif isinstance(item, EditableLargeNumberMenuItem):
-            return MenuItemFormatter._format_large_num_for_display(item, data)
+            return MenuItemFormatter._format_large_num_for_display(data)
         elif isinstance(item, EditableTextMenuItem):
-            return MenuItemFormatter._format_text_for_display(item, data)
+            return MenuItemFormatter._format_text_for_display(data)
         elif isinstance(item, Rgb32MenuItem):
-            return MenuItemFormatter._format_rgb_item_for_display(item, data)
+            return MenuItemFormatter._format_rgb_item_for_display(data)
         elif isinstance(item, ScrollChoiceMenuItem):
-            return MenuItemFormatter._format_scroll_item_for_display(item, data)
+            return MenuItemFormatter._format_scroll_item_for_display(data)
         else:
             return ""
 
     @staticmethod
-    def _format_scroll_item_for_display(item: ScrollChoiceMenuItem, data: CurrentScrollPosition) -> str:
+    def _format_scroll_item_for_display(data: CurrentScrollPosition) -> str:
         return data.value
 
     @staticmethod
-    def _format_rgb_item_for_display(item: Rgb32MenuItem, data: PortableColor) -> str:
+    def _format_rgb_item_for_display(data: PortableColor) -> str:
         return str(data)
 
     @staticmethod
-    def _format_text_for_display(item: EditableTextMenuItem, data: str) -> str:
+    def _format_text_for_display(data: str) -> str:
         return data
 
     @staticmethod
-    def _format_large_num_for_display(item: EditableLargeNumberMenuItem, data: int) -> str:
+    def _format_large_num_for_display(data: int) -> str:
         return str(data)
 
     @staticmethod
--- test/domain/util/test_menu_item_formatter.py
+++ test/domain/util/test_menu_item_formatter.py
@@ -1,8 +1,94 @@
-from tcmenu.domain.menu_items import AnalogMenuItem
+import pytest
+
+from tcmenu.domain.edit_item_type import EditItemType
+from tcmenu.domain.menu_items import AnalogMenuItem, Rgb32MenuItem
 from tcmenu.domain.state.current_scroll_position import CurrentScrollPosition
 from tcmenu.domain.state.menu_tree import MenuTree
 from tcmenu.domain.util.menu_item_formatter import MenuItemFormatter
+from tcmenu.domain.util.menu_item_helper import MenuItemHelper
 from test.domain.domain_fixtures import DomainFixtures
+
+
+def test_format_for_wire():
+    tree: MenuTree = DomainFixtures.full_esp_amplifier_test_tree()
+
+    assert MenuItemFormatter.format_to_wire(tree.get_menu_by_id(1), "-39.5dB") == "-39.5dB"
+    assert MenuItemFormatter.format_to_wire(tree.get_menu_by_id(2), "3") == "3-3"
+    assert MenuItemFormatter.format_to_wire(tree.get_menu_by_id(2), "abc") == "0-abc"
+    assert MenuItemFormatter.format_to_wire(tree.get_menu_by_id(3), "ON") == "1"
+    assert MenuItemFormatter.format_to_wire(tree.get_menu_by_id(3), "yes") == "1"
+    assert MenuItemFormatter.format_to_wire(tree.get_menu_by_id(3), "false") == "0"
+    assert MenuItemFormatter.format_to_wire(tree.get_menu_by_id(4), "asdf") == "0"
+
+    with pytest.raises(ValueError):
+        MenuItemFormatter.format_to_wire(tree.get_menu_by_id(5), "")
+
+    with pytest.raises(ValueError):
+        MenuItemFormatter.format_to_wire(tree.get_menu_by_id(24), "")
+
+    assert MenuItemFormatter.format_to_wire(tree.get_menu_by_id(22), "text") == "text"
+    assert MenuItemFormatter.format_to_wire(tree.get_menu_by_id(22), "text_longer_than_defined_length") == ""
+    assert (
+        MenuItemFormatter.format_to_wire(
+            MenuItemHelper.create_from_existing(tree.get_menu_by_id(22), item_type=EditItemType.IP_ADDRESS),
+            "192.168.1.10",
+        )
+        == "192.168.1.10"
+    )
+    assert (
+        MenuItemFormatter.format_to_wire(
+            MenuItemHelper.create_from_existing(tree.get_menu_by_id(22), item_type=EditItemType.IP_ADDRESS),
+            "192x168x1x10",
+        )
+        == "0.0.0.0"
+    )
+    assert (
+        MenuItemFormatter.format_to_wire(
+            MenuItemHelper.create_from_existing(tree.get_menu_by_id(22), item_type=EditItemType.TIME_12H), "12:34:56"
+        )
+        == "12:34:56"
+    )
+    assert (
+        MenuItemFormatter.format_to_wire(
+            MenuItemHelper.create_from_existing(tree.get_menu_by_id(22), item_type=EditItemType.TIME_24H),
+            "12:34:56.123",
+        )
+        == "12:34:56.123"
+    )
+    assert (
+        MenuItemFormatter.format_to_wire(
+            MenuItemHelper.create_from_existing(tree.get_menu_by_id(22), item_type=EditItemType.TIME_24_HUNDREDS),
+            "12:34 56",
+        )
+        == "12:00:00"
+    )
+    assert (
+        MenuItemFormatter.format_to_wire(
+            MenuItemHelper.create_from_existing(tree.get_menu_by_id(22), item_type=EditItemType.GREGORIAN_DATE),
+            "01/08/2023",
+        )
+        == "01/08/2023"
+    )
+    assert (
+        MenuItemFormatter.format_to_wire(
+            MenuItemHelper.create_from_existing(tree.get_menu_by_id(22), item_type=EditItemType.GREGORIAN_DATE),
+            "01/08 2023",
+        )
+        == "01/01/2000"
+    )
+    assert (
+        MenuItemFormatter.format_to_wire(
+            MenuItemHelper.create_from_existing(tree.get_menu_by_id(22), item_type=EditItemType.TIME_24H_HHMM),
+            "12:34:56.123",
+        )
+        == ""
+    )
+
+    assert MenuItemFormatter.format_to_wire(Rgb32MenuItem(name="RGB"), "#ababab") == "#ABABABFF"
+    assert MenuItemFormatter.format_to_wire(Rgb32MenuItem(name="RGB"), "test") == "#000000FF"
+    assert MenuItemFormatter.format_to_wire(DomainFixtures.a_large_number("large_num", 210, 5, False), "1000") == "1000"
+    assert MenuItemFormatter.format_to_wire(DomainFixtures.a_large_number("large_num", 210, 5, False), "a1000") == "0"
+    assert MenuItemFormatter.format_to_wire(tree.get_menu_by_id(14), "abc") == "abc"
 
 
 def test_format_for_display():
